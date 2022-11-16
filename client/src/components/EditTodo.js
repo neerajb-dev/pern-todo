fdd8f7e4bd2af74e72d5ae9bc1dc33b5eec91c6b
Neeraj Bhardwaj <neerajb@yahoo.com>
2022-11-17 01:33:01 +0530

    pern todo app frontend added

---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const EditTodo = ({ todo }) => {
+    const [description, setDescription] = useState(todo.description);
+
+    // edit description function
+    const updateDescription = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { description };
+            const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            });
+            window.location = "/";
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    return (
+        <>
+            <button type="button" className="btn btn-warning" data-bs-toggle="modal" data-bs-target={`#id${todo.todo_id}`}>
+                Edit
+            </button>
+
+            <div className="modal fade" id={`id${todo.todo_id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" onClick={() => setDescription(todo.description)}>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Edit Todo</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setDescription(todo.description)}></button>
+                        </div>
+                        <div className="modal-body">
+                            <input type="text" className="form-control" value={description} onChange={(e) => setDescription(e.target.value)} />
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-warning" onClick={(e) => updateDescription(e)}>Edit</button>
+                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={() => setDescription(todo.description)}>Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default EditTodo;
